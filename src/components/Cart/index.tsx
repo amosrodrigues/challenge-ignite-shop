bbca2b3cedb2446ebb7e18cbc12251551c13dd3b
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-09-21 11:18:50 -0300

    feat: implement show cart.

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,18 +1,69 @@
 import Link from 'next/link';
-import { Handbag } from 'phosphor-react';
+import { Handbag, X } from 'phosphor-react';
+import { useState } from 'react';
 import { theme } from '../../styles';
-import { CartButton, CartContainer } from '../../styles/components/cart';
+import {
+  CartButtonClose,
+  CartButtonOpen,
+  CartCardItem,
+  CartContainer,
+  CartContent,
+  CartItemSumary,
+  ImageContainer,
+  ItemDetails,
+} from '../../styles/components/cart';
 
 export function Cart() {
+  const [showCart, setShowCart] = useState(false);
+
+  function handleToggleShowCart() {
+    setShowCart(!showCart);
+  }
+
   return (
     <CartContainer>
-      <CartButton>
-        <Handbag
-          size={24}
-          color={true ? String(theme.colors['gray300']) : '#8D8D99'}
-        />
+      <CartButtonOpen onClick={handleToggleShowCart} disabled={false}>
+        <Handbag size={24} />
         <span>1</span>
-      </CartButton>
+      </CartButtonOpen>
+
+      {showCart && (
+        <CartContent>
+          <CartButtonClose onClick={handleToggleShowCart}>
+            <X size={24} weight="bold" />
+          </CartButtonClose>
+
+          <strong>Sacola de compras</strong>
+
+          <CartCardItem>
+            <ImageContainer></ImageContainer>
+            <ItemDetails>
+              <h1>Camiseta Beyond the Limits</h1>
+              <span>R$ 79,90</span>
+              <button>Remover</button>
+            </ItemDetails>
+          </CartCardItem>
+
+          <CartItemSumary>
+            <tbody>
+              <tr>
+                <td>Total de itens</td>
+                <td>R$ 79,90</td>
+              </tr>
+              {/* <tr>
+              <td>Entrega</td>
+              <td>{priceFormatter.format(deliveryFee)}</td>
+            </tr> */}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td>Valor Total</td>
+                <td>R$ 79,90</td>
+              </tr>
+            </tfoot>
+          </CartItemSumary>
+        </CartContent>
+      )}
     </CartContainer>
   );
 }
--- src/styles/components/cart.ts
+++ src/styles/components/cart.ts
@@ -1,11 +1,11 @@
-import { styled } from '..';
+import { keyframes, styled } from '..';
 
 export const CartContainer = styled('div', {
   flex: 1,
   display: 'flex',
 });
 
-export const CartButton = styled('button', {
+export const CartButtonOpen = styled('button', {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
@@ -18,6 +18,16 @@ export const CartButton = styled('button', {
   background: '$gray800',
   borderRadius: 6,
   border: 0,
+
+  svg: {
+    color: '$gray300',
+  },
+
+  '&:disabled': {
+    svg: {
+      color: '$gray700',
+    },
+  },
 
   span: {
     width: '1.5rem',
@@ -39,5 +49,132 @@ export const CartButton = styled('button', {
     fontWeight: 'bold',
     fontSize: '$sm',
     lineHeight: 1.6,
+  },
+});
+
+export const CartButtonClose = styled('button', {
+  border: 0,
+  lineHeight: 0,
+  padding: 0,
+  position: 'absolute',
+  top: '1.5rem',
+  right: '1.5rem',
+
+  background: 'transparent',
+  color: '$gray700',
+  cursor: 'pointer',
+});
+
+const moveToLeft = keyframes({
+  '0%': { transform: 'translateX(110%)' },
+  '100%': { transform: 'translateX(0%)' },
+});
+
+export const CartContent = styled('div', {
+  width: '480px',
+  minHeight: '100vh',
+  position: 'fixed',
+  right: 0,
+  top: 0,
+  bottom: 0,
+  zIndex: 99,
+
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '3rem',
+
+  background: '$gray800',
+  boxShadow: '-4px 0px 30px rgba(0, 0, 0, 0.8)',
+  animation: `${moveToLeft} 300ms ease-in-out`,
+
+  strong: {
+    fontSize: '$lg',
+    lineHeight: 1.6,
+    marginTop: '1.5rem',
+  },
+});
+
+export const CartCardItem = styled('div', {
+  display: 'flex',
+  gap: '1.25rem',
+  marginTop: '2rem',
+  overflowY: 'auto',
+});
+
+export const ImageContainer = styled('div', {
+  width: '100%',
+  maxWidth: 101.94,
+  height: 93,
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  padding: '0.25rem',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover',
+  },
+});
+
+export const ItemDetails = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+
+  gap: '0.5rem',
+  justifyContent: 'center',
+
+  h1: {
+    fontSize: '$md',
+    color: '$gray300',
+    fontWeight: 'normal',
+  },
+
+  span: {
+    display: 'block',
+    fontSize: '$md',
+    fontWeight: 'bold',
+    color: '$gray100',
+    lineHeight: 1.6,
+  },
+
+  button: {
+    lineHeight: 1.6,
+    maxWidth: 'fit-content',
+    cursor: 'pointer',
+    border: 0,
+    background: 'transparent',
+    color: '$green500',
+    fontWeight: 'bold',
+  },
+});
+
+export const CartItemSumary = styled('table', {
+  width: '100%',
+  lineHeight: 1.6,
+  marginTop: 'auto',
+
+  'tbody tr td': {
+    fontSize: '$sm',
+    color: '$gray300',
+  },
+
+  'td:nth-child(even)': {
+    textAlign: 'right',
+  },
+
+  'tfoot tr td': {
+    fontWeight: 'bold',
+    color: '$gray100',
+  },
+
+  'tfoot tr td:nth-child(1)': {
+    fontSize: '$md',
+  },
+
+  'tfoot tr td:nth-child(2)': {
+    fontSize: '$xl',
   },
 });
